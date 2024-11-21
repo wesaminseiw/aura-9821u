2a505857bda7527b0efe59a9167645d0439ee14f
unknown <wesam.inseiw99@gmail.com>
2024-11-21 12:54:00 +0200

    added-logos-to-assets-and-their-paths-to-contants.dart-also-added-themes-extension-getters

---
+++ lib/app/utils/constants.dart
@@ -0,0 +1,9 @@
+//* LOGOS PATHS
+
+//^ app logos (aura)
+const logoDark = 'assets/logos/logo-dark.png';
+const logoLight = 'assets/logos/logo-light.png';
+const logoDarkNoBg = 'assets/logos/logo-dark-no-bg.png';
+const logoLightNoBg = 'assets/logos/logo-light-no-bg.png';
+const logoDarkRounded = 'assets/logos/logo-dark-rounded.png';
+const logoLightRounded = 'assets/logos/logo-light-rounded.png';
